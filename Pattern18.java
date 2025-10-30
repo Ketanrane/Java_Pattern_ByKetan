3ffef499138beaf21452199e5802afc1478b04e9
Ketan Rane <raneketan1231@gmail.com>
2025-10-30 22:18:23 +0530

    Refactor Pattern18.java for clarity and simplicity
    Removed commented-out pattern and simplified loop structure.

--- Pattern18.java
+++ Pattern18.java
@@ -12,19 +12,20 @@
 
         for (int i = 5; i >= 1; i--) {
             // Printing spaces before stars
-            for (int j = 1; j <= (5 - i); j++) {
+            for (int j = 1; j <= (5 - i); j++) 
                 System.out.print("  ");
-            }
+           
             // Printing stars in the first half of the row
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= i; j++) 
                 System.out.print("* ");
-            }
+            
             // Printing stars in the second half of the row
-            for (int j = 1; j < i; j++) {
+            for (int j = 1; j < i; j++) 
                 System.out.print("* ");
-            }
+            
             // Moving to the next line after each row
             System.out.println();
         }
     }
 }
+
